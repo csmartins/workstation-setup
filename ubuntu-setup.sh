a6698ff1798ec64a3caadd6c35bed550c2f057c5
Carlos Martins <csmartins.ufrj@gmail.com>
2020-05-04 22:22:35 -0300

    Install code

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -4,8 +4,10 @@ echo "+++++++++++++++++++++++++++ Updating system for no surprises +++++++++++++
 sudo apt update
 
 echo "+++++++++++++++++++++++++++ Installing some awesome tools for you, awesome developer +++"
-sudo apt install -y git curl google-chrome-stable
+sudo apt install -y git curl google-chrome-stable pwgen
+sudo snap install --classic code
 
 echo "+++++++++++++++++++++++++++ Now I'm gonna make this terminal awesome +++++++++++++++++++"
 sudo apt install -y git-core zsh tmux
 sudo sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+zsh
