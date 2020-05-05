ef71f798e3ba09def7f512e3ffbff1a989ac6034
Carlos Martins <csmartins.ufrj@gmail.com>
2020-05-04 22:46:51 -0300

    Update bashrc

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -7,12 +7,13 @@ echo "+++++++++++++++++++++++++++ Installing some awesome tools for you, awesome
 sudo apt install -y git curl google-chrome-stable pwgen
 sudo snap install --classic code
 
-echo "+++++++++++++++++++++++++++ Now I'm gonna make this terminal awesome +++++++++++++++++++"
-sudo apt install -y git-core zsh tmux
-sudo sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-zsh
-
-echo "+++++++++++++++++++++++++++ Installing some additional cool things +++++++++++++++++++"
+echo "+++++++++++++++++++++++++++ Installing some additional cool things +++++++++++++++++++++"
 sudo snap install vlc
 wget -O stremio.deb https://dl.strem.io/linux/v4.4.106/stremio_4.4.106-1_amd64.deb
 sudo apt install ./stremio.deb
+
+echo "+++++++++++++++++++++++++++ Now I'm gonna make this terminal awesome +++++++++++++++++++"
+sudo apt install -y git-core zsh tmux
+sudo sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+echo zsh >> ~/.bashrc
+echo tmux >> ~/.bashrc
