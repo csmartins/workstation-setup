861669e6d60b877b4f89b5bb2a20303c06afd7bc
Carlos Martins <csmartins.ufrj@gmail.com>
2020-05-04 22:06:29 -0300

    Add tmux and zsh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-sudo -i
-
+echo "Updating system for no surprises"
 sudo apt update
-sudo apt install git
+
+echo "Installing some awesome tools for you, awesome developer"
+sudo apt install git curl google-chrome-stable
+
+echo "Now I'm gonna make this terminal awesome"
+sudo apt install tmux
+sudo sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
