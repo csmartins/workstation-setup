df4346cf9ad5bcd297247eced58a2808b301e726
Carlos Martins <csmartins.ufrj@gmail.com>
2020-05-04 22:11:03 -0300

    Improve log

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Updating system for no surprises"
+echo "########################### Updating system for no surprises ###########################"
 sudo apt update
 
-echo "Installing some awesome tools for you, awesome developer"
-sudo apt install git curl google-chrome-stable
+echo "########################### Installing some awesome tools for you, awesome developer ###"
+sudo apt install -y git curl google-chrome-stable
 
-echo "Now I'm gonna make this terminal awesome"
-sudo apt install tmux
+echo "########################### Now I'm gonna make this terminal awesome ###################"
+sudo apt install -y tmux
 sudo sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
