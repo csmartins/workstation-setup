f1816ff5980b5fb5794ea4be39ab7aebf16eca69
Carlos Martins <csmartins.ufrj@gmail.com>
2020-05-04 23:00:06 -0300

    Change final

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -15,9 +15,5 @@ sudo apt install ./stremio.deb
 echo "+++++++++++++++++++++++++++ Now I'm gonna make this terminal awesome +++++++++++++++++++"
 sudo apt install -y git-core zsh tmux
 sudo sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-echo tmux >> ~/.bashrc
-echo zsh >> ~/.bashrc
-echo clear >> ~/.bashrc
 
-echo "+++++++++++++++++++++++++++ Brilha +++++++++++++++++++++++++++++++++++++++++++++++++++++"
-gnome-terminal
+echo "Add tmux and zsh commands to your ~/.bashrc, one at each line, so you don't need to open tmux manualy"
