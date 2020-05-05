e8cc158b57c24cf02b9583661a4beb1ec2f2d37f
Carlos Martins <csmartins.ufrj@gmail.com>
2020-05-04 22:14:56 -0300

    install oh my zsh pre-reqs

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -7,5 +7,5 @@ echo "+++++++++++++++++++++++++++ Installing some awesome tools for you, awesome
 sudo apt install -y git curl google-chrome-stable
 
 echo "+++++++++++++++++++++++++++ Now I'm gonna make this terminal awesome +++++++++++++++++++"
-sudo apt install -y tmux
+sudo apt install -y git-core zsh tmux
 sudo sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
