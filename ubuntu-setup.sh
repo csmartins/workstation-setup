abbb841cab9c113409fd30de7c8bf29502e8cf2b
Carlos Martins <csmartins.ufrj@gmail.com>
2020-05-17 13:25:52 -0300

    Add base dev folders

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -12,6 +12,10 @@ sudo snap install vlc
 wget -O stremio.deb https://dl.strem.io/linux/v4.4.106/stremio_4.4.106-1_amd64.deb
 sudo apt install ./stremio.deb
 
+echo "+++++++++++++++++++++++++++ Creating some base development folders +++++++++++++++++++++"
+mkdir -p ~/projects/challenges
+mkdir -p ~/projects/study
+
 echo "+++++++++++++++++++++++++++ Now I'm gonna make this terminal awesome +++++++++++++++++++"
 sudo apt install -y git-core zsh tmux
 sudo sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
