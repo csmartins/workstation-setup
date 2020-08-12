d330da2b7548aade11424899d41b3d47e8c3b20d
Carlos Martins <csmartins.ufrj@gmail.com>
2020-08-12 08:29:39 -0300

    docker compose installation steps

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -29,6 +29,9 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 echo "+++++++++++++++++++++++++++ Installing tfevn +++++++++++++++++++++++++++++++++++++++++++"
 
 git clone https://github.com/tfutils/tfenv.git ~/.tfenv
