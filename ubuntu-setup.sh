ee8e16435724ea23f4036e8544a18ec7db262e88
Carlos Martins <csmartins.ufrj@gmail.com>
2021-01-10 11:35:24 -0300

    Install postman

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -32,6 +32,8 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+sudo snap install postman
+
 echo "+++++++++++++++++++++++++++ Installing tfevn +++++++++++++++++++++++++++++++++++++++++++"
 
 git clone https://github.com/tfutils/tfenv.git ~/.tfenv
