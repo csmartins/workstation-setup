6fa78aec8087a7f0b3efc742f2a7a9aa264bf38b
Cadu Martins <csmartins.personal@gmail.com>
2021-05-23 12:53:38 -0300

    Begin setup for quantum chemistry workstation

---
+++ quantum-chemistry-setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "+++++++++++++++++++++++++++ Updating system for no surprises +++++++++++++++++++++++++++"
+sudo apt update
+
+echo "+++++++++++++++++++++++++++ Installing some awesome tools for you, awesome developer +++"
+sudo apt install -y git curl pwgen
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo snap install --classic code
+
+echo "+++++++++++++++++++++++++++ Installing some awesome pythonic tools +++++++++++++++++++++"
+sudo apt install software-properties-common python-software-properties
+sudo add-apt-repository ppa:pypa/ppa
+sudo apt update
+sudo apt install python3-pip -y
+pip3 install --user pipenv
+export PATH="$PATH:$HOME/.local/bin"
+curl https://pyenv.run | bash
+export PATH="$HOME/.pyenv/bin:$PATH"
+
+echo "+++++++++++++++++++++++++++ Installing chemestry tools ++++++++++++++++++++++++++++++++"
+echo "+++++++++++++++++++++++++++ Installing Quantum Espresso 6.5 +++++++++++++++++++++++++++"
+wget -O q-e.tgz https://github.com/QEF/q-e/releases/download/qe-6.5/qe-6.5-ReleasePack.tgz
+sudo tar -xzvf q-e.tgz -C /opt/
+sudo /opt/qe-6.5/configure
+sudo make -C /opt/qe-6.5/ all
+echo "+++++++++++++++++++++++++++ Installing Xcrysden +++++++++++++++++++++++++++++++++++++++"
+echo "+++++++++++++++++++++++++++ Installing Xmgrace ++++++++++++++++++++++++++++++++++++++++"
